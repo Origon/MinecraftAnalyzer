4339d78d7202f5b3526ac99d0f43aae72dcc1976
Keith Stein <origon.stein@gmail.com>
2020-07-06 12:21:08 -0400

    Add project files.

---
+++ MinecraftAnalyzer/Analyzer.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MiNET.LevelDB;
+using MiNET.LevelDB.Utils;
+using NUnit.Framework;
+using fNbt;
+using System.Diagnostics;
+using System.Windows.Media.Media3D;
+using System.Threading;
+
+namespace MinecraftAnalyzer
+{
+    public static class Analyzer
+    {
+        public static void ParseChunkIfExists(Database db, ChunkCoordinate coordinates, Action<BlockInfo> blockAction)
+        {
+            for (byte y = 0; y < 16; y++)
+            {
+                ParseSubChunkIfExists(db, new SubChunkCoordinate(coordinates, y), blockAction);
+            }
+        }
+
+        public static Task ParseChunkIfExistsAsync(Database db, ChunkCoordinate coordinates, Action<BlockInfo> blockAction)
+        {
+            return Task.Run(() =>
+            {
+                var subChunkTasks = new List<Task>();
+
+                for (byte y = 0; y < 16; y++)
+                {
+                    subChunkTasks.Add(ParseSubChunkIfExistsAsync(db, new SubChunkCoordinate(coordinates, y), blockAction));
+                }
+
+                Task.WaitAll(subChunkTasks.ToArray());
+            });
+        }
+
+        public static void ParseSubChunkIfExists(Database db, SubChunkCoordinate coordinates, Action<BlockInfo> blockAction)
+        {
+            var chunkDataKey = BitConverter.GetBytes(coordinates.X).Concat(BitConverter.GetBytes(coordinates.Z)).Concat(new byte[] { 0x2f, (byte)coordinates.Y }).ToArray();
+            Monitor.Enter(db);
+            var chunkData = db.Get(chunkDataKey);
+            Monitor.Exit(db);
+            if (chunkData != null) { ParseSubChunkData(chunkData, coordinates, blockAction); }
+        }
+
+        public static Task ParseSubChunkIfExistsAsync(Database db, SubChunkCoordinate coordinates, Action<BlockInfo> blockAction)
+        {
+            return Task.Run(() => ParseSubChunkIfExists(db, coordinates, blockAction));
+        }
+
+        public static void ParseSubChunkData(byte[] data, SubChunkCoordinate coordinates, Action<BlockInfo> blockAction)
+        {
+            //Format details found at
+            //https://minecraft.gamepedia.com/Bedrock_Edition_level_format
+
+            var reader = new SpanReader(data);
+
+            //Make sure we're using the version this code was designed for
+            var version = reader.ReadByte();
+            Assert.AreEqual(8, version);
+
+            var storageSize = reader.ReadByte();
+            for (int i = 0; i < storageSize; i++)
+            {
+                //Number of bits used to store the palette index of a single block
+                var bitsPerBlock = reader.ReadByte() >> 1;
+                //Number of blocks that fit into a single "word" (a 32-bit, unsigned integer)
+                int blocksPerWord = (int)Math.Floor(32 / (double)bitsPerBlock);
+                //Number of words we expect to find
+                int numberOfWords = (int)Math.Ceiling(4096 / (double)blocksPerWord);
+                //Read as many bytes as that many words would take up (each word being 4 bytes)
+                var blockData = reader.Read(numberOfWords * 4);
+
+                //The number of different types of blocks stored in the palette for this subchunk
+                int paletteSize = reader.ReadInt32();
+
+                //Read each palette item into a list
+                var palette = new List<NbtFile>();
+                for (int j = 0; j < paletteSize; j++)
+                {
+                    NbtFile file = new NbtFile();
+                    file.BigEndian = false;
+                    var buffer = ((ReadOnlySpan<byte>)data).Slice(reader.Position).ToArray();
+
+                    int numberOfBytesRead = (int)file.LoadFromStream(new MemoryStream(buffer), NbtCompression.None);
+                    reader.Position += numberOfBytesRead;
+                    palette.Add(file);
+                    Assert.NotZero(numberOfBytesRead);
+                }
+
+                //We'll need to read each block index into a byte array buffer so we can convert it to an int and use it to look up a value in the palette list.
+                //Valid values for bitsPerBlock are 1, 2, 3, 4, 5, 6, 8 and 16.
+                //So for 16 we use 16 bits (2 bytes) and for anything else we use 8 bits (1 byte), since we know all other valid values are one byte or less.
+                int blockBufferSize = bitsPerBlock == 16 ? 16 : 8;
+
+                var dataReader = new SpanReader(blockData);
+                int blockNumber = 0;
+
+                //Loop until we reach either the expected number of words.
+                for (int wordNumber = 0; wordNumber < numberOfWords; wordNumber++)
+                {
+                    //Read the next word (4 bytes; a single 32 bit integer)
+                    var word = new BitArray(dataReader.Read(4).ToArray());
+
+                    //Current index for reading bits in the current word
+                    int wordIndex = 0;
+
+                    //Loop until there could be no more full block indexes in the current word
+                    //(i.e. the next block index would take us past the end of the word),
+                    //or until the maximum number of blocks in a subchunk (16*16*16=4096) is reached.
+                    //This leaves off the padding for bitsPerBlock which aren't factors of 32.
+                    while (!(wordIndex + bitsPerBlock > word.Length) && blockNumber < 4096)
+                    {
+                        var blockBuffer = new BitArray(blockBufferSize);
+
+                        //Read however many bits constitutes a block id into the buffer.
+                        //The rest of the buffer will stay 0, which is perfect since everything is little-endian.
+                        for (int bufferIndex = 0; bufferIndex < bitsPerBlock; bufferIndex++)
+                        {
+                            blockBuffer.Set(bufferIndex, word.Get(wordIndex));
+                            wordIndex++;
+                        }
+
+                        int paletteIndex = -1;
+
+                        //For an 8-bit buffer, we copy to a byte and cast to int
+                        if (blockBufferSize == 8)
+                        {
+                            byte[] bytes = new byte[1];
+                            blockBuffer.CopyTo(bytes, 0);
+                            paletteIndex = bytes[0];
+                        }
+                        //For a 16-bit buffer, we copy to two bytes, interpret it as a 16-bit int and then cast to int.
+                        else if (blockBufferSize == 16)
+                        {
+                            byte[] bytes = new byte[2];
+                            blockBuffer.CopyTo(bytes, 0);
+                            paletteIndex = BitConverter.ToUInt16(bytes);
+                        }
+
+                        //Blocks are stored by Y then Z then X, so use the overall index to get the 3D coordinates using math.
+                        //These initial coordinates will be relative to the subchunk, not the absolute coordinates of that block in the world.
+                        int y = blockNumber % 16;
+                        int z = (blockNumber / 16) % 16;
+                        int x = (int)Math.Truncate(blockNumber / (double)(16 * 16));
+
+                        //The initial coordinates will be relative to the chunk.
+                        //We need to add them to the chunks coordinates to get the absolute position of the block in the world.
+                        x += coordinates.X;
+                        z += coordinates.Z;
+                        //The Y coordinate is actually which slice of the chunk (0-15) is being parsed, not the real Y coordniate of where the slice starts.
+                        //Since each slice is 16 blocks tall, we need to multiple by 16 before adding to get the correct Y of the block.
+                        y = (coordinates.Y * 16) + y;
+
+                        var block = new BlockInfo(palette[paletteIndex], new Point3D(x, y, z));
+                        blockAction.Invoke(block);
+
+                        blockNumber++;
+                    }
+                }
+            }
+        }
+
+        public static Task ParseSubChunkDataAsync(byte[] data, SubChunkCoordinate coordinates, Action<BlockInfo> blockAction)
+        {
+            return Task.Run(() => ParseSubChunkData(data, coordinates, blockAction));
+        }
+    }
+
+
+
+    public struct ChunkCoordinate
+    {
+        public ChunkCoordinate(int x, int z)
+        {
+            X = x;
+            Z = z;
+        }
+
+        public int X;
+        public int Z;
+    }
+
+    public struct SubChunkCoordinate
+    {
+        public SubChunkCoordinate(int x, byte y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public SubChunkCoordinate(ChunkCoordinate chunk, byte y)
+        {
+            X = chunk.X;
+            Y = y;
+            Z = chunk.Z;
+        }
+
+        public int X;
+        public byte Y;
+        public int Z;
+    }
+
+    public class BlockInfo
+    {
+        public BlockInfo() { }
+
+        public BlockInfo(NbtFile state, Point3D coords)
+        {
+            State = state;
+            Coordinates = coords;
+        }
+
+        public NbtFile State { get; }
+        public Point3D Coordinates { get; }
+    }
+}
---
+++ MinecraftAnalyzer/MainWindow.xaml.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using MiNET.LevelDB;
+using NUnit.Framework;
+using CsvHelper;
+using System.Globalization;
+using Microsoft.Win32;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+
+namespace MinecraftAnalyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool Running
+        {
+            get { return (bool)GetValue(RunningProperty); }
+            private set { SetValue(RunningPropertyKey, value); }
+        }
+        public static readonly DependencyPropertyKey RunningPropertyKey =
+            DependencyProperty.RegisterReadOnly("Running", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
+        public static readonly DependencyProperty RunningProperty = RunningPropertyKey.DependencyProperty;
+
+        public string InputFile
+        {
+            get { return (string)GetValue(InputFileProperty); }
+            set { SetValue(InputFileProperty, value); }
+        }
+        public static readonly DependencyProperty InputFileProperty =
+            DependencyProperty.Register("InputFile", typeof(string), typeof(MainWindow));
+
+        public string OutputFile
+        {
+            get { return (string)GetValue(OutputFileProperty); }
+            set { SetValue(OutputFileProperty, value); }
+        }
+        public static readonly DependencyProperty OutputFileProperty =
+            DependencyProperty.Register("OutputFile", typeof(string), typeof(MainWindow));
+
+        private void B_BrowseForImput_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Minecraft Bedrock World (*.mcworld)|*.mcworld";
+
+            if ((bool)dialog.ShowDialog())
+            {
+                InputFile = dialog.FileName;
+            }
+        }
+
+        private void B_BrowseForOutput_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Filter = "Comma-seperated text file (*.csv)|*.csv";
+
+            if ((bool)dialog.ShowDialog())
+            {
+                OutputFile = dialog.FileName;
+            }
+        }
+
+        public static readonly RoutedCommand AnalyzeCommand = new RoutedCommand("Analyze", typeof(MainWindow));
+
+        private void CanExecuteAnalyze(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.Handled = true;
+            e.CanExecute = !Running && InputFile != null && OutputFile != null;
+        }
+
+        private async void ExecuteAnalyze(object sender, ExecutedRoutedEventArgs e)
+        {
+            e.Handled = true;
+            Running = true;
+            var s = Stopwatch.StartNew();
+            await AnalyzeChunksAroundCenterAsync(20, true);
+            s.Stop();
+            MessageBox.Show($"Completed after {Math.Floor(s.Elapsed.TotalMinutes)} minute(s) and {s.Elapsed.Seconds} second(s).");
+            Running = false;
+        }
+
+        public void AnalyzeChunksAroundCenter(int chunksRadiusToCheck, bool multiThread = true)
+        {
+            Assert.IsTrue(BitConverter.IsLittleEndian);
+
+            string inputFile = null;
+            string outputFile = null;
+
+            if (Dispatcher.CheckAccess())
+            {
+                inputFile = InputFile;
+                outputFile = OutputFile;
+            }
+            else
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    inputFile = InputFile;
+                    outputFile = OutputFile;
+                });
+            }
+
+            using (var db = new Database(new DirectoryInfo(inputFile)))
+            using (var baseWriter = new StreamWriter(outputFile, false))
+            using (var csvWriter = new CsvWriter(baseWriter, CultureInfo.CurrentCulture))
+            {
+                db.Open();
+
+                csvWriter.Configuration.RegisterClassMap<BlockInfoCsvMap>();
+
+                BlockQueue = new ConcurrentQueue<BlockInfo>();
+
+                var chunkTasks = new List<Task>();
+
+                for (int x = 0; x < chunksRadiusToCheck; x++)
+                {
+                    for (int z = 0; z < chunksRadiusToCheck; z++)
+                    {
+                        if (multiThread)
+                        {
+                            chunkTasks.Add(Analyzer.ParseChunkIfExistsAsync(db, new ChunkCoordinate(x, z), QueueBlock));
+                        }
+                        else
+                        {
+                            Analyzer.ParseChunkIfExists(db, new ChunkCoordinate(x, z), QueueBlock);
+                        }                        
+
+                        if (z > 0) 
+                        {
+                            if (multiThread)
+                            {
+                                chunkTasks.Add(Analyzer.ParseChunkIfExistsAsync(db, new ChunkCoordinate(x, z * -1), QueueBlock));
+                            }
+                            else
+                            {
+                                Analyzer.ParseChunkIfExists(db, new ChunkCoordinate(x, z * -1), QueueBlock);
+                            }                                
+                        }
+
+                        if (x > 0)
+                        {
+                            if (multiThread)
+                            {
+                                chunkTasks.Add(Analyzer.ParseChunkIfExistsAsync(db, new ChunkCoordinate(x * -1, z), QueueBlock));
+                            }
+                            else
+                            {
+                                Analyzer.ParseChunkIfExists(db, new ChunkCoordinate(x * -1, z), QueueBlock);
+                            }
+                        }
+
+                        if (x > 0 || z > 00)
+                        {
+                            if (multiThread)
+                            {
+                                chunkTasks.Add(Analyzer.ParseChunkIfExistsAsync(db, new ChunkCoordinate(x * -1, z * -1), QueueBlock));
+                            }
+                            else
+                            {
+                                Analyzer.ParseChunkIfExists(db, new ChunkCoordinate(x * -1, z * -1), QueueBlock);
+                            }
+                        }
+                    }
+                }
+
+                Task.WaitAll(chunkTasks.ToArray());
+
+                csvWriter.WriteRecords(BlockQueue);
+            }
+        }
+
+        public Task AnalyzeChunksAroundCenterAsync(int chunksRadiusToCheck, bool multiThread = true)
+        {
+            return Task.Run(() => AnalyzeChunksAroundCenter(chunksRadiusToCheck, multiThread));
+        }
+
+        public class BlockInfoCsvMap : CsvHelper.Configuration.ClassMap<BlockInfo>
+        {
+            public BlockInfoCsvMap()
+            {
+                Map(b => b.Coordinates, false).Name("X").ConvertUsing(row => row.Coordinates.X.ToString());
+                Map(b => b.Coordinates, false).Name("Y").ConvertUsing(row => row.Coordinates.Y.ToString());
+                Map(b => b.Coordinates, false).Name("Z").ConvertUsing(row => row.Coordinates.Z.ToString());
+                Map(b => b.State, false).Name("Block").ConvertUsing(row => row.State.RootTag["name"].StringValue);
+            }
+        }
+
+        private string[] oreBlocks = { "minecraft:gold_ore",
+                                       "minecraft:iron_ore",
+                                       "minecraft:coal_ore",
+                                       "minecraft:lapis_ore",
+                                       "minecraft:diamond_ore",
+                                       "minecraft:redstone_ore",
+                                       "minecraft:lit_redstone_ore",
+                                       "minecraft:emerald_ore",
+                                       "minecraft:lava",
+                                       "minecraft:flowing_lava"};
+
+        private ConcurrentQueue<BlockInfo> BlockQueue;
+
+        private void QueueBlock(BlockInfo block)
+        {
+            if (oreBlocks.Contains(block.State.RootTag["name"].StringValue))
+            {
+                BlockQueue.Enqueue(block);
+            }
+        }
+    }
+}
